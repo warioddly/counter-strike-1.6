cbcdc8947d64815327886dbbb72c1e87a4844c49
Bekeev Imankadyr <53710487+warioddly@users.noreply.github.com>
2023-12-15 13:23:15 +0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+    entry: './src/main.ts',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: 'index.html' }),
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)$/i,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [stylesHandler, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|jpg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: '/assets/images/[name][ext]',
+                },
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: '/assets/videos/[name][ext]',
+                },
+            },
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: '/assets/shaders/[name][ext]',
+                },
+            },
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
